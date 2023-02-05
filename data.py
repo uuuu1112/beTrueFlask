30695a69b52a94c9f96da10696ee26f19ed660b9
曾卓明 <cengzhuoming@cengzhuomingdeMac-mini.local>
2023-02-05 23:17:03 +0800

    async func

--- data.py
+++ data.py
@@ -1,5 +1,6 @@
 from basic import *
-import threading
+import concurrent.futures
+# import threading
 
 class API(FromAPI):
     def __init__(self,stock_id):
@@ -12,11 +13,27 @@ class API(FromAPI):
         self.priceTable=pd.DataFrame([])
         self.financialTable=pd.DataFrame([])
         self.dividendTable=pd.DataFrame([])
+        self.basicData=self.getBasicData()
     def revenueApi(self):
-        return self.singleStock(self.stock_id,self.start_date,self.today,"TaiwanStockMonthRevenue")
+        self.revenueTable=self.singleStock(self.stock_id,self.start_date,self.today,"TaiwanStockMonthRevenue")
     def priceApi(self):
-        return self.singleStock(self.stock_id,self.start_date,self.today,"TaiwanStockPrice")    
+        self.priceTable=self.singleStock(self.stock_id,self.start_date,self.today,"TaiwanStockPrice")    
     def financialApi(self):
-        return self.singleStock(self.stock_id,self.start_date,self.today,"TaiwanStockFinancialStatements")    
+        self.financialTable=self.singleStock(self.stock_id,self.start_date,self.today,"TaiwanStockFinancialStatements")    
     def dividendApi(self):
-        return self.singleStock(self.stock_id,self.start_date,self.today,"TaiwanStockDividend")
+        self.dividendTable=self.singleStock(self.stock_id,self.start_date,self.today,"TaiwanStockDividend")
+    def getBasicData(self):
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            future_revenue=executor.submit(self.revenueApi)
+            future_price=executor.submit(self.priceApi)
+            futrue_financial=executor.submit(self.financialApi)
+            future_dividend=executor.submit(self.dividendApi)
+
+            future_revenue.result()
+            future_price.result()
+            futrue_financial.result()
+            future_dividend.result()
+
+class BasicTable(API):
+    def __init__(self,stock_id):
+        super().__init__(stock_id)    
